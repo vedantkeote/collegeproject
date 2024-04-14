a8806d6ebbdf6ddc5ddcf48d4fc965626d189cb2
vedthegreat <vedthegreat2004@gmail.com>
2024-04-14 15:05:49 +0530

    completed login page

---
+++ js/login.js
@@ -0,0 +1,6 @@
+function toggle(){
+    const register=document.querySelector('.registerform');
+    const login=document.querySelector('.loginform');
+    register.classList.toggle('notactive');
+    login.classList.toggle('notactive');
+}
